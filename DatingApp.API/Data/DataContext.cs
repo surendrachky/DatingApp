b23f8fed83026c0b5ced7757e4b70087056bedc7
surendrachksy <surendrachksy@gmail.com>
2020-08-14 17:48:00 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+         {
+            //Database.EnsureCreated();
+         }   
+
+        public DbSet<Value> Values { get; set; }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+       // {
+          //  modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        //}
+    }
+}
